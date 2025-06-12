f538108d36f7c86653e8ab56e07230aaa69eeefa
Azzevedo <galegovski@gmx.com>
2025-06-11 22:22:19 -0300

    Adicionados testes na classe Program.cs

--- Program.cs
+++ Program.cs
@@ -1,3 +1,14 @@
 ﻿using DesafioPOO.Models;
 
-// TODO: Realizar os testes com as classes Nokia e Iphone
+// [x]: Realizar os testes com as classes Nokia e Iphone
+Console.WriteLine("Smartphone Nokia:");
+Smartphone nokia = new Nokia("2345678", "Nokia 1100", "7777777", 256);
+nokia.Ligar();
+nokia.InstalarAplicativo("Snake");
+
+Console.WriteLine("\n");
+
+Console.WriteLine($"AiPonei");
+Smartphone aiPonei = new Iphone("753951232", "Ponei 13", "222222222", 32);
+aiPonei.ReceberLigacao();
+aiPonei.InstalarAplicativo("Jailbreaker");
