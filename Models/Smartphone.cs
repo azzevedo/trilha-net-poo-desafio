5aa1882ffded658edcda9daf45857ff67ea31793
Azzevedo <galegovski@gmx.com>
2025-06-11 22:05:37 -0300

    Classe Smartphone implementada

--- Models/Smartphone.cs
+++ Models/Smartphone.cs
@@ -3,12 +3,18 @@ namespace DesafioPOO.Models
     public abstract class Smartphone
     {
         public string Numero { get; set; }
-        // TODO: Implementar as propriedades faltantes de acordo com o diagrama
+		// [x]: Implementar as propriedades faltantes de acordo com o diagrama
+		private string Modelo { get; }
+		private string IMEI { get; }
+		private int Memoria { get; }
 
-        public Smartphone(string numero)
-        {
-            Numero = numero;
-            // TODO: Passar os parâmetros do construtor para as propriedades
+		public Smartphone(string numero, string modelo, string imei, int memoria)
+		{
+			Numero = numero;
+			// [x]: Passar os parâmetros do construtor para as propriedades
+			Modelo = modelo;
+			IMEI = imei;
+			Memoria = memoria;
         }
 
         public void Ligar()
